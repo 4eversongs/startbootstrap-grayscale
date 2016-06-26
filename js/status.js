2fc2c40fb6ca06aa2678ffd60e2cddd4c0b68ec3
4eversongs <stevepunchline@gmail.com>
2016-06-26 17:12:20 -0500

    Create status.js

---
+++ js/status.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+  $(document).ready(function(){
+    $.stratus({
+      auto_play: true,
+      download: false,
+      key: "Enter your Client ID here",
+      links: 'http://soundcloud.com/qotsa',
+      random: true
+    });
+  });
+</script>
