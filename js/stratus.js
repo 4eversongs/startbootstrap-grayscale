64f2a6012a1ffdfc61eb4f5d5a5b535d84d56309
4eversongs <stevepunchline@gmail.com>
2016-06-26 18:04:38 -0500

    Update stratus.js

--- js/stratus.js
+++ js/stratus.js
@@ -3,6 +3,7 @@
     $.stratus({
       auto_play: true,
       download: false,
+      align: "bottom",
       key: "abbe78d4b54c56709e5e7a241196390d",
       links: 'http://soundcloud.com/4ever-songs',
       random: true
