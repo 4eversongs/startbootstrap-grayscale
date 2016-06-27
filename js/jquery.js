562e85ef2403c2b84b6076d8b530d0441d300fa9
4eversongs <stevepunchline@gmail.com>
2016-06-27 17:14:33 -0500

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $('.nav').css('background-color', 'rgba(34,34,34,0.9)');
+          $('.nav').css('background-color', 'rgba(34,34,34,1)');
        } else {
           $('.nav').css('background-color', 'transparent');
        }
